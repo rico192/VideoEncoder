11677c5124ea0842b3029e348715ccbdceedfae0
rico192 <ricoananda.9c26@gmail.com>
2021-07-17 23:37:10 +0700

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -13,7 +13,7 @@ async def starter(event):
     await event.reply(
         f"Hi {user.first_name}\nI can encode Telegram files in x265, just send me a video.",
         buttons=[
-            Button.url("Channel", url="https://t.me/BotzHub"),
+            Button.url("Channel", url="https://t.me/kangothersc"),
             Button.url("Source", url="https://github.com/xditya/video-encoder-bot"),
         ],
     )
