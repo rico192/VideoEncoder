6926a325c941564831ad55df786660cb44076b52
rico192 <ricoananda.9c26@gmail.com>
2021-07-20 13:50:41 +0700

    Make 60 fps

--- VideoEncoder/helpers/ffmpeg_utils.py
+++ VideoEncoder/helpers/ffmpeg_utils.py
@@ -50,7 +50,7 @@ def encode(filepath):
             video_opts = "-c:v copy -tag:v hvc1"
     else:
         # Transcode to h265 / hvc1
-        video_opts = "-c:v libx265 -crf 32 -tag:v hvc1 -preset faster"
+        video_opts = "-c:v libx265 -crf 31 -tag:v hvc1 -preset faster -filter:v minterpolate=fps=60"
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel="a:0")
     if audio_codec == []:
